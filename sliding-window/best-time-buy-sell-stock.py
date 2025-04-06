08aad346b9eb905e32c6392bc0387251e7ba246c
Fuzzabee <jamesjbaker@tutanota.com>
2025-04-06 05:23:12 -0400

    Complete best-time-buy-sell-stock.py

---
+++ sliding-window/best-time-buy-sell-stock.py
@@ -0,0 +1,35 @@
+# You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+# You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+# Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+def maxProfit(prices):
+    if len(prices) == 0: return 0
+    
+    buy = prices[0]
+    sell = prices[0]
+    profit = sell - buy
+    for n in prices:
+        if n < buy:
+            buy = n
+            sell = n
+            continue
+        if n > sell:
+            sell = n
+            if sell - buy > profit:
+                profit = sell - buy
+    
+    return profit
+
+def main():
+    # Input: prices = [10,1,5,6,7,1]
+    # Output: 6
+    prices = [10,1,5,6,7,1]
+    print(maxProfit(prices))
+
+    # Input: prices = [10,8,7,5,2]
+    # Output: 0
+    prices = [10,8,7,5,2]
+    print(maxProfit(prices))
+
+if __name__ == "__main__":
+    main()
