d7632b8084a3275fadb24cfa701d70ade6c8a669
Fuzzabee <jamesjbaker@tutanota.com>
2025-04-12 16:06:38 -0400

    Update tracker.ods and complete maximum-depth-of-binary-tree.py

--- trees/maximum-depth-of-binary-tree.py
+++ trees/maximum-depth-of-binary-tree.py
@@ -15,10 +15,25 @@ class TreeNode:
         self.right = right
 
 def maxDepth(root):
-    return 0
+    if not root: return 0
+    return calculateDepth([root], 0)
+
+def calculateDepth(queue, depth):
+    if not queue:
+        return depth
+    depth += 1
+    newQueue = []
+    while queue:
+        nextNode = queue.pop()
+        if nextNode.left:
+            newQueue.append(nextNode.left)
+        if nextNode.right:
+            newQueue.append(nextNode.right)
+    return calculateDepth(newQueue, depth)
 
 def main():
-    # root = [1,2,3,null,null,4]
+    # Input: root = [1,2,3,null,null,4]
+    # Output: 3
     n1 = TreeNode(1)
     n2 = TreeNode(2)
     n3 = TreeNode(3)
@@ -26,6 +41,23 @@ def main():
     n1.left = n2
     n1.right = n3
     n3.left = n4
+    print(maxDepth(n1))
+
+    # Input: root = [1,null,2]
+    # Output: 2
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n1.right = n2
+    print(maxDepth(n1))
+
+    # Input: root = [0]
+    # Output: 1
+    n1 = TreeNode(0)
+    print(maxDepth(n1))
+
+    # Input: root = []
+    # Output: 0
+    n1 = None
     print(maxDepth(n1))
 
 if __name__ == "__main__":
