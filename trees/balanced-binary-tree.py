51ef155469b2be7277ab0d0a1650ba76dce98df6
Fuzzabee <jamesjbaker@tutanota.com>
2025-04-15 05:29:34 -0400

    Create skeleton for balanced-binary-tree.py

---
+++ trees/balanced-binary-tree.py
@@ -0,0 +1,50 @@
+# Given a binary tree, return true if it is height-balanced and false otherwise.
+# A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def getHeight(root):
+    return -1
+
+def isBalanced(root):
+    return False
+
+def main():
+    # Input: root = [1,2,3,null,null,4]
+    # Output: true
+    n1 = TreeNode(1)
+    n2 = TreeNode(2)
+    n3 = TreeNode(3)
+    n4 = TreeNode(4)
+    n5 = TreeNode(5)
+
+    n1.left = n2
+    n1.right = n3
+    n3.left = n4
+
+    print(isBalanced(n1))
+
+    # Input: root = [1,2,3,null,null,4,null,5]
+    # Output: false
+    n4.left = n5
+    print(isBalanced(n1))
+
+    # Input: root = []
+    # Output: true
+    n1 = None
+    print(isBalanced(n1))
+
+
+if __name__ == "__main__":
+    main()
