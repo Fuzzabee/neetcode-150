f2168164e223247f37318e679fde82d9018e6561
Fuzzabee <jamesjbaker@tutanota.com>
2025-07-15 17:35:40 -0400

    Create skeleton for climbing-stairs.py

---
+++ 1d-dynamic-programming/climbing-stairs.py
@@ -0,0 +1,22 @@
+### INSTRUCTIONS ###
+# You are given an integer n representing the number of steps to reach the top of a staircase. You can climb with either 1 or 2 steps at a time.
+#
+# Return the number of distinct ways to climb to the top of the staircase.
+#
+# Input: n = 2
+# Output: 2
+#
+# Input: n = 3
+# Output: 3
+####################
+
+
+def climbStairs(n):
+    return -1
+
+def main():
+    print(climbStairs(2))
+    print(climbStairs(3))
+
+if __name__ == "__main__":
+    main()
